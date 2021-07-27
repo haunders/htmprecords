47106ad00d220e79315eb312cc30296edbe84dcc
haunders <66288374+haunders@users.noreply.github.com>
2021-07-27 19:16:23 +0500

    оптимизация

---
+++ artists/script.js
@@ -0,0 +1,35 @@
+function removeAll() {
+	document.getElementById('chart').style.display = "none"
+	document.getElementById('albums').style.display = "none"
+	document.getElementById('ep').style.display = "none"
+	document.getElementById('singles').style.display = "none"
+	document.getElementById('part').style.display = "none"
+	document.getElementById('bg').style.display = "none"
+}
+function changePage(select) {
+	var i = select.options[select.selectedIndex];
+	if (i.value == "Альбомы") {
+		removeAll()
+		document.getElementById('albums').style.display = "flex"
+	}
+	else if (i.value == "EP") {
+		removeAll()
+		document.getElementById('ep').style.display = "flex"
+	}
+	else if (i.value == "Синглы") {
+		removeAll()
+		document.getElementById('singles').style.display = "flex"
+	}
+	else if (i.value == "Участие в альбомах") {
+		removeAll()
+		document.getElementById('part').style.display = "flex"
+	}
+	else if (i.value == "Популярные треки") {
+		removeAll()
+		document.getElementById('chart').style.display = "flex"
+		document.getElementById('bg').style.display = "block"
+	}
+	else {
+		removeAll()
+	}
+}
